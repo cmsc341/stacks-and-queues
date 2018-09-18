77d710fc7c7a27ed5d41aa88002fae945486ead0
Ben <doc.ben.johnson@gmail.com>
2018-09-18 13:47:46 -0400

    section 5

--- queues/Queue.cpp
+++ queues/Queue.cpp
@@ -1,18 +1,46 @@
 //
 // Created by Ben Johnson on 9/16/18.
 //
-
+// Magdalene "Maggie" Proclaimation: start points to whatever was enqueued least recently, end points to whatever was enqueued most recently
 #include "Queue.h"
 
 Queue::Queue(int capacity) {
     array = new int[capacity];
+    this->capacity = capacity;
+    size = 0;
+    start = -1;
+    end = -1;
 }
 
 Queue::~Queue() {
+    delete [] array;
+    array = 0;
+}
 
+
+void Queue::enqueue(int data) {
+    if (isEmpty()) {
+        start++;
+    }
+    if (isFull()) {
+        throw "no thanks, nerd";
+    }
+    end++;
+    array[end] = data;
+    size++;
 }
 
 int Queue::dequeue() {
     // we have to shift the array back to fill empty space
-    return 0;
+    if (isEmpty()) {
+        throw "an exception";
+    }
+    start++;
+    size--;
+    for (int i = 0; i < size - 1; ++i) {
+        array[i] = array[i + 1];
+        // what the shift? O(1)
+    }
+    return 5;
 }
+
--- stacks/Stack.cpp
+++ stacks/Stack.cpp
@@ -1,1 +1,31 @@
 #include "Stack.h"
+// Emma edict: topIndex points to the last item pushed
+Stack::Stack(int capacity) {
+    array = new int[capacity];
+    topIndex = -1;
+    this->capacity = capacity;
+}
+
+Stack::~Stack() {
+    delete [] array;
+    array = 0;
+}
+
+void Stack::push(int i) {
+    if (topIndex + 1 == capacity) {
+        throw "can't push to a full stack, brah?";
+    }
+    topIndex++;
+    array[topIndex] = i;
+}
+
+int Stack::pop() {
+    if (isEmpty()) {
+        throw "Or return 0.";
+    }
+    topIndex--;
+    return array[topIndex + 1];
+}
+
+
+
