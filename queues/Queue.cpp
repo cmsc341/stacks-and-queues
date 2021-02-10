615e14f702475c451735c92b6d6f682fee34d4bb
Prof J <benj1@umbc.edu>
2021-02-10 10:36:04 -0500

    implemented enqueue

--- queues/Queue.cpp
+++ queues/Queue.cpp
@@ -2,10 +2,14 @@
 // Created by Ben Johnson on 9/16/18.
 //
 
+#include <exception>
 #include "Queue.h"
 
 Queue::Queue(int capacity) {
     array = new int[capacity];
+    start = 0;
+    end = 0;
+    size = 0;
 }
 
 Queue::~Queue() {
@@ -15,4 +19,13 @@ Queue::~Queue() {
 int Queue::dequeue() {
     // we have to shift the array back to fill empty space
     return 0;
+}
+
+void Queue::enqueue(int data) {
+    if (size == capacity) {
+        throw std::exception();
+    }
+    array[end] = data;
+    end++;
+    size++;
 }
--- queues/main.cpp
+++ queues/main.cpp
@@ -9,8 +9,18 @@ int main() {
     q.enqueue(4);
     q.enqueue(2);
     q.enqueue(8);
-    cout << q.dequeue() << endl;
-    cout << q.dequeue() << endl;
+    int value = q.dequeue();
+    if (value == 2) {
+        cout << "Pass!" << endl;
+    } else {
+        cout << "Fail!" << endl;
+    }
+    cout << value << endl; // prints 2
+    if (value == 4) {
+        cout << "Pass!" << endl;
+    } else {
+        cout << "Fail!" << endl;
+    }
     q.enqueue(3);
     cout << q.dequeue() << endl;
     return 0;
